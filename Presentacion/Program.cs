45349811b97aab4a0db1664eaf8787aef6aa8a63
egsoluciones <r.eduardo.gtz@gmail.com>
2022-02-24 15:58:03 -0700

    Cambio de form principal

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -16,7 +16,7 @@ namespace Presentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmChecador());
+            Application.Run(new FrmLogin());
         }
     }
 }
