7d6d44d74c23134337d86a283426e6fa950f3352
egsoluciones <r.eduardo.gtz@gmail.com>
2022-03-04 09:22:46 -0700

    Actualización con Menú en Base de datos local y remota

---
+++ DatosRH/DTO/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosRH.DTO
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+    }
+}
